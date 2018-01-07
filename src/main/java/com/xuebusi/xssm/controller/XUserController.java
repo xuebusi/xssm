af7d564ae888c1bb9022e766c522dd96c21e34ca
xbs1019 <xbs1019@126.com>
2018-01-07 16:19:34 +0800

    优化导包

--- src/main/java/com/xuebusi/xssm/controller/XUserController.java
+++ src/main/java/com/xuebusi/xssm/controller/XUserController.java
@@ -1,14 +1,14 @@
 package com.xuebusi.xssm.controller;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.xuebusi.xssm.common.XResult;
 import com.xuebusi.xssm.pojo.XUser;
-import com.xuebusi.xssm.pojo.XUserExample;
 import com.xuebusi.xssm.service.XUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
