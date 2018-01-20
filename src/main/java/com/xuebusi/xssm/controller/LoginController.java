1e00d038e8e0da917715d9591b1d1f8f7ccad3e3
史艳军 <490983587@qq.com>
2018-01-20 14:48:52 +0800

    微信授权登陆

---
+++ src/main/java/com/xuebusi/xssm/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.xuebusi.xssm.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.belerweb.social.bean.Result;
+import com.belerweb.social.weixin.api.Weixin;
+import com.belerweb.social.weixin.bean.AccessToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.belerweb.social.weixin.bean.User;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: syj
+ * @CreateDate: 2018/1/20 13:46
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController extends BaseController {
+
+    private static String appid = "";
+    private static String secret = "";
+
+    @RequestMapping(value = "/wx")
+    @ResponseBody
+    public User loginByWeChat(HttpServletRequest request) {
+        String code = request.getParameter("code");
+        Weixin weixin = new Weixin(appid, secret);
+        //根据code获取access_token
+        Result<AccessToken> accessTokenResult = weixin.getOAuth2().accessToken(code);
+        AccessToken accessToken = accessTokenResult.getResult();
+        //刷新 access_token
+        //Result<AccessToken> newAccessTokenResult = weixin.getOAuth2().refreshAccessToken(accessToken.getRefreshToken());
+        //根据access_token获取用户信息
+        Result<User> userResult = weixin.getUser().snsapiUserInfo(accessToken.getToken(), accessToken.getOpenId());
+        User user = userResult.getResult();
+        System.out.println(JSON.toJSONString(user));
+        return user;
+    }
+}
