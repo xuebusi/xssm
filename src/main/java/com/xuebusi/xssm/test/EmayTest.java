92f0d529f8ac2aabf16fde8e0cdbc1b486acd20c
xuebusi <xbs1019@126.com>
2018-04-29 21:07:33 +0800

    将亿美彩信接口返回结果转成json

--- src/main/java/com/xuebusi/xssm/test/EmayTest.java
+++ src/main/java/com/xuebusi/xssm/test/EmayTest.java
@@ -1,6 +1,11 @@
 package com.xuebusi.xssm.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.xuebusi.xssm.common.util.HttpClientUtil;
+import com.xuebusi.xssm.common.util.Xml2Json;
+import org.dom4j.DocumentException;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,14 +22,14 @@ public class EmayTest {
         System.out.println(result1);
 
         // 测试查询余额接口
-        String result2 = getMMSCount();
-        System.out.println("------------查询余额接口返回结果--------------");
-        System.out.println(result2);
+//        String result2 = getMMSCount();
+//        System.out.println("------------查询余额接口返回结果--------------");
+//        System.out.println(result2);
 
         // 测试查询账户状态接口
-        String result3 = getStatusReport();
-        System.out.println("------------查询账户状态接口返回结果--------------");
-        System.out.println(result3);
+//        String result3 = getStatusReport();
+//        System.out.println("------------查询账户状态接口返回结果--------------");
+//        System.out.println(result3);
 
     }
 
@@ -52,8 +57,25 @@ public class EmayTest {
 
         // 发送彩信
         String result = HttpClientUtil.sendPost(reqURL, paramMap, "UTF-8", "application/x-www-form-urlencoded");
-
-        return result;
+        // 最终返回的结果存到map
+        Map<String, String> resultMap = new HashMap<>();
+        try {
+            // 将xml转成json对象
+            JSONObject jsonObject = Xml2Json.xml2Json(result);
+            if (jsonObject != null) {
+                // 获取key为string的部分
+                String resultStr = (String) jsonObject.get("string");
+                // 根据亿美的文档，如果以OK开头，表示彩信发送成功
+                if (!StringUtils.isEmpty(resultStr) && resultStr.startsWith("OK")) {
+                    resultMap.put("result", "true");
+                    return JSON.toJSONString(resultMap);
+                }
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        resultMap.put("result", "false");
+        return JSON.toJSONString(resultMap);
     }
 
     /**
@@ -79,8 +101,16 @@ public class EmayTest {
 
         // 查询账户余额
         String result = HttpClientUtil.sendPost(reqURL, paramMap, "UTF-8", "application/x-www-form-urlencoded");
-
-        return result;
+        try {
+            // 将xml转成json对象
+            JSONObject jsonObject = Xml2Json.xml2Json(result);
+            if (jsonObject != null) {
+                return jsonObject.toString();
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
@@ -112,7 +142,26 @@ public class EmayTest {
 
         // 查询账户状态
         String result = HttpClientUtil.sendPost(reqURL, paramMap, "UTF-8", "application/x-www-form-urlencoded");
-
-        return result;
+        if (!StringUtils.isEmpty(result)) {
+            // 由于返回的不是标准的xml格式，需要处理成标准的xml格式才能正确解析：
+            // (1)将其中所欲的&gt;替换成>号;
+            // (2)将所有的&lt;替换成<号;
+            // (3)并将其中的<?xml version="1.0"?>部分删除;
+            String replaceResult = result.replace("&gt;", ">").replace("&lt;", "<").replace("<?xml version=\"1.0\"?>", "");
+            try {
+                // 使用工具类将xml转成json对象
+                JSONObject jsonObject = Xml2Json.xml2Json(replaceResult);
+                if (jsonObject != null) {
+                    // 将名为MMSResult的部分解析出来并返回
+                    Object mmsResult = jsonObject.get("MMSResult");
+                    if (mmsResult != null) {
+                        return mmsResult.toString();
+                    }
+                }
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
