185782aae0248c5e010a88ad60656b44ff4b8480
史艳军 <qq490983587>
2018-05-12 10:36:48 +0800

    亿美soap发送彩信demo测试

--- src/main/java/com/emy/mms/TestMMSDemo.java
+++ src/main/java/com/emy/mms/TestMMSDemo.java
@@ -2,36 +2,36 @@ package com.emy.mms;
 
 import com.emy.mms.client.MMSClient;
 import com.emy.mms.client.SingletonClient;
-import org.springframework.util.StringUtils;
 
 import java.io.FileInputStream;
 
 public class TestMMSDemo {
 
 	public static void main(String[] args) {
-		try {
-			String mmsTitle = "测试亿美发送彩信接口";
-			String userNumbers = "15801081566";
-			int sendType = 1;
-			byte[] content = readContent();
-			String result = sendMMS(mmsTitle, userNumbers, content, sendType);
-			System.out.println(result);
-			if (!StringUtils.isEmpty(result) && result.startsWith("OK")) {
-				System.out.println("彩信发送成功=====================");
-			} else {
-				System.out.println("彩信发送失败=====================");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// 测试发送彩信
+		System.out.println(sendMMS());
+		System.out.println("====================");
+
+		// 测试查询余额
+		System.out.println(testGetBalance());
+		System.out.println("====================");
+
+		// 测试获得状态报告
+		System.out.println(testGetReport());
+
+
 	}
 	
 	/**
 	 * 发送彩信
 	 */
-	public static String sendMMS(String mmsTitle, String userNumbers, byte[] content, int sendType) {
+	public static String sendMMS() {
 		String result = null;
 		try {
+			String mmsTitle = "测试亿美发送彩信接口";
+			String userNumbers = "15801081566";
+			int sendType = 1;
+			byte[] content = readContent();
 			String userName = "bf-test";
 			String password = "c5f1213ce8";
 			MMSClient mmsClient = SingletonClient.getClient(userName, password);
@@ -65,27 +65,34 @@ public class TestMMSDemo {
 	 * 查询余额
 	 * 1是网关账户，2是点卡账户
 	 */
-	public static void testGetBalance() {
+	public static Long testGetBalance() {
+		Long mmsCount = null;
 		try {
 			String userName = "bf-test";
 			String password = "c5f1213ce8";
-			System.out.println(SingletonClient.getClient(userName, password).getMMSCount(1));
+			System.out.println();
+			MMSClient mmsClient = SingletonClient.getClient(userName, password);
+			mmsCount = mmsClient.getMMSCount(1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return mmsCount;
 	}
 	
 	/**
 	 * 获得状态报告
 	 */
-	public static void testGetReport() {
+	public static String testGetReport() {
+		String statusReport = null;
 		try {
 			String userName = "bf-test";
 			String password = "c5f1213ce8";
-			System.out.println(SingletonClient.getClient(userName, password).getStatusReport());
+			MMSClient mmsClient = SingletonClient.getClient(userName, password);
+			statusReport = mmsClient.getStatusReport();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return statusReport;
 	}
 
 }
