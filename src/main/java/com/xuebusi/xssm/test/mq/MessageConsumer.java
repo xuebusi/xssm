e8073dcfddd931d8dcfbe932a2121ec505de583c
xuebusi <xbs1019@126.com>
2018-07-17 23:09:33 +0800

    消息队列测试类优化

--- src/main/java/com/xuebusi/xssm/test/mq/MessageConsumer.java
+++ src/main/java/com/xuebusi/xssm/test/mq/MessageConsumer.java
@@ -1,5 +1,6 @@
 package com.xuebusi.xssm.test.mq;
 
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -12,6 +13,8 @@ public class MessageConsumer<T> implements Runnable {
 
     private BlockingQueue<T> messageQueue;
 
+    private static final int SLEEPTIME = 500;
+
     public MessageConsumer(BlockingQueue<T> messageQueue) {
         this.messageQueue = messageQueue;
     }
@@ -21,13 +24,15 @@ public class MessageConsumer<T> implements Runnable {
      */
     @Override
     public void run() {
+        Random random = new Random();
         while (true) {
             try {
-                Thread.sleep(50);
                 String message = (String) messageQueue.take();
                 System.out.println(Thread.currentThread().getName() + "==========消费消息:" + message);
+                Thread.sleep(random.nextInt(SLEEPTIME));
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
--- src/main/java/com/xuebusi/xssm/test/mq/MessageProducer.java
+++ src/main/java/com/xuebusi/xssm/test/mq/MessageProducer.java
@@ -1,7 +1,8 @@
 package com.xuebusi.xssm.test.mq;
 
-import java.util.UUID;
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 消息生产者
@@ -11,7 +12,13 @@ import java.util.concurrent.BlockingQueue;
  */
 public class MessageProducer<T> implements Runnable {
 
+    private volatile boolean isRunning = true;
+
+    private static AtomicInteger count = new AtomicInteger();
+
     private BlockingQueue<T> messageQueue;
+
+    private static final int SLEEPTIME = 500;
 
     public MessageProducer(BlockingQueue<T> messageQueue) {
         this.messageQueue = messageQueue;
@@ -22,15 +29,21 @@ public class MessageProducer<T> implements Runnable {
      */
     @Override
     public void run() {
-        while (true) {
+        Random random = new Random();
+        while (isRunning) {
             try {
-                Thread.sleep(50);
-                String message = UUID.randomUUID().toString();
+                Thread.sleep(random.nextInt(SLEEPTIME));
+                String message = String.valueOf(count.incrementAndGet());
                 messageQueue.put((T) message);
                 System.out.println(Thread.currentThread().getName() + "==========生产消息:" + message);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
+    }
+
+    public void stop() {
+        this.isRunning = false;
     }
 }
