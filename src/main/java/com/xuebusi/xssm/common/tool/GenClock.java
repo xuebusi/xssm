bc18158658a9ca0dd1b83ed320f13e906e1d8296
v_shiyanjun <v_shiyanjun@baidu.com>
2018-11-04 09:50:51 +0800

    分钟支持最小值

--- src/main/java/com/xuebusi/xssm/common/tool/GenClock.java
+++ src/main/java/com/xuebusi/xssm/common/tool/GenClock.java
@@ -15,7 +15,7 @@ public class GenClock {
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        List<String> list = gen(20, 9, 22, 56, 47);
+        List<String> list = gen(20, 9, 22, 56, 32, 27, 12);
         for (String s : list) {
             System.out.println(s);
         }
@@ -24,24 +24,34 @@ public class GenClock {
     /**
      * 生成打卡记录
      *
-     * @Author shiyanjun
-     * @Date 2018/10/27 下午8:59
-     * @Param days 生成天数
-     * @Param amHour 生成早上小时部分
-     * @Param pmHour 生成晚上小时部分
-     * @Param amMaxMinute 生成早上分钟部分
-     * @Param pmMaxMinute 生成晚上分钟部分
-     * @Return
-     * @Exception
+     * @author shiyanjun
+     * @date 2018/10/27 下午8:59
+     * @param days 生成天数
+     * @param amHour 早上小时部分
+     * @param pmHour 晚上小时部分
+     * @param amMaxMinute 早上分钟最大值
+     * @param amMinMinute 早上分钟最小值
+     * @param pmMaxMinute 晚上分钟最大值
+     * @param pmMinMinute 晚上分钟最小值
+     * @return
+     * @exception
      */
-    public static List<String> gen(int days, int amHour, int pmHour, int amMaxMinute, int pmMaxMinute) {
+    public static List<String> gen(int days, int amHour, int pmHour, int amMaxMinute, int amMinMinute, int pmMaxMinute, int pmMinMinute) {
         String h1 = String.valueOf(amHour);
         String h2 = String.valueOf(pmHour);
 
         List<String> list = new ArrayList<>();
         for (int i = 0; i < days; i++) {
-            int r1 = random.nextInt(amMaxMinute);
-            int r2 = random.nextInt(pmMaxMinute);
+
+            int r1 = 0;
+            while (r1 < amMinMinute) {
+                r1 = random.nextInt(amMaxMinute);
+            }
+
+            int r2 = 0;
+            while (r2 < pmMinMinute) {
+                r2= random.nextInt(pmMaxMinute);
+            }
 
             String s1 = String.valueOf(r1);
             String s2 = String.valueOf(r2);
